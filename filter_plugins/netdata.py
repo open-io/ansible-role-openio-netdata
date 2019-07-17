db6e05230f327fbbbdb45905ddbaac37cb2ae90f
Soeren MEDARD <soeren.medard@outlook.com>
2019-07-17 17:49:13 +0900

    CHANGE(netdata): Add the rack in the host tags
     ##### SUMMARY
    Add the rack to the host tags so it can be used by the web UI.
    Also do some cleanup and simplification.
    
     ##### IMPACT
    N/A if the refactoring went well.

--- filter_plugins/netdata.py
+++ filter_plugins/netdata.py
@@ -1,6 +1,3 @@
-import os
-import re
-
 import io
 from base64 import b64decode
 try:
@@ -12,41 +9,8 @@ except ImportError:
 class FilterModule(object):
     def filters(self):
         return {
-            'services_by_path': self.services_by_path,
-            'beanstalks_by_status2': self.beanstalks_by_status2,
-            'ip_port': self.ip_port,
             'aws_creds': self.aws_creds
         }
-
-    def services_by_path(self, paths):
-        services = list()
-
-        for path in paths:
-            path_splitted = path.split(os.sep)
-            ns = path_splitted[4]
-            service_type, id = path_splitted[5].split('-')
-            services.append({'ns': ns, 'service_type': service_type, 'id': id})
-        return services
-
-    def beanstalks_by_status2(self, command_lines):
-        services = list()
-
-        for line in command_lines:
-            line_splitted = re.split(" +", line)
-            ns, service_type, id = line_splitted[0].split('-')
-            ip = line_splitted[10]
-            port = line_splitted[12]
-            services.append({'ns': ns, 'service_type': service_type, 'id': id,
-                             'ip': ip, 'port': port})
-        return services
-
-    def ip_port(self, ip_port):
-        services = list()
-
-        for pair in ip_port:
-            ip, port = pair.split(':')
-            services.append({'ip': ip, 'port': port})
-        return services
 
     def aws_creds(self, data, type_='access'):
         """ Retrieves AWS creds from b64 encoded data received by slurp """
