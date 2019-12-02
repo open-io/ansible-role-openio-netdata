ca40ffe875bbcad8488074d7f031ee4ed4d1e768
Vladimir DOMBROVSKI <vladimir@openio.io>
2019-12-02 15:40:09 +0100

    CHANGE(netdata): Replace python.d redis collector with netdata plugin
     ##### SUMMARY
    
    The redis.plugin is available since openio-netdata-plugins 0.6.0, this
    enables it in netdata.
    
     ##### IMPACT
    
    The new redis collector exports metrics under different measurement
    names; Grafana dashboads need to be provisioned for the 19.10 release
    
     ##### ADDITIONAL INFORMATION

--- filter_plugins/netdata.py
+++ filter_plugins/netdata.py
@@ -11,7 +11,8 @@ except ImportError:
 class FilterModule(object):
     def filters(self):
         return {
-            'aws_creds': self.aws_creds
+            'aws_creds': self.aws_creds,
+            'redis_roles': self.redis_roles
         }
 
     def aws_creds(self, data, type_='access'):
@@ -25,3 +26,27 @@ class FilterModule(object):
         if type_ == 'secret':
             return config.get('default', 'aws_secret_access_key')
         return config.get('default', 'aws_access_key_id')
+
+    def redis_roles(self, services):
+        """
+        Organise redis services into clusters for the redis collector
+        ---
+        Example:
+        services = [
+            {"ip": "10.10.10.11", "port": 6011, "role": "account"},
+            {"ip": "10.10.10.11", "port": 6411, "role": "ch"},
+            {"ip": "10.10.10.11", "port": 6511, "role": "bdb"}
+        ]
+        self.redis_roles(services)
+        > "10.10.10.11:6011:account,10.10.10.11:6411:ch,10.10.10.11:6511:bdb"
+        """
+        tmp = dict()
+        res = []
+        for service in services:
+            tmp.setdefault(service['role'], [])
+            tmp[service['role']].append(service)
+        for idx, cluster in enumerate(tmp.values()):
+            for service in cluster:
+                res.append("%s:%d:%s" %
+                           (service['ip'], service['port'], service['role']))
+        return ",".join(res)
